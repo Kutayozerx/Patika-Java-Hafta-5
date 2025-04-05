ca4d396f396af47fb1713df9ef72acea5f90802b
Kutayozer17 <kutayozer04@gmail.com>
2025-04-05 14:21:04 +0300

    hafta 5

--- src/beyond_classes_enums/EnumÖdev/Starter.java
+++ src/beyond_classes_enums/EnumÖdev/Starter.java
@@ -8,8 +8,25 @@ public class Starter {
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("Lütfen bir gün girin: ");
-        scanner.nextLine();
+        String input = scanner.nextLine();
 
+        Day selectedDay = getDayFromInput(input);
 
+        if (selectedDay != null) {
+            System.out.println(selectedDay + " günü çalışma saatleri: " + selectedDay.getWorkingHours());
+        } else {
+            System.out.println("Geçersiz gün girdiniz. Lütfen geçerli bir gün adı girin.");
+        }
+
+        scanner.close();
+    }
+
+    private static Day getDayFromInput(String input) {
+        for (Day day : Day.values()) {
+            if (day.name().equalsIgnoreCase(input)) {
+                return day;
+            }
+        }
+        return null;
     }
 }
